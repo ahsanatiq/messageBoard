fc50905370431ab2c800c0dcb4c8c40ee1d83877
Ahsan <iamahsan82@gmail.com>
2017-03-16 17:43:24 +0500

    custom error messages

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+<div class="container text-center">
+    <h1><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span></h1>
+    <h1>Un-authorized. Not Allowed.</h1>
+</div>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+<div class="container text-center">
+    <h1><span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span></h1>
+    <h1>Page Not Found</h1>
+</div>
