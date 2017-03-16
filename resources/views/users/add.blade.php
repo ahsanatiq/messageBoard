209a3c29e8833cf5775fd6e03c4e47750db21e67
Ahsan <iamahsan82@gmail.com>
2017-03-16 19:05:17 +0500

    fix cancel button

--- resources/views/users/add.blade.php
+++ resources/views/users/add.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.nav')
+    @include('layouts.nav')
     <div class="container">
         <h1>Create User</h1>
         {!! Form::open(['route'=>'users.create', 'id'=>'js-form']) !!}
-        @include('users._user')
+        @include('users._user',['action'=>'create'])
         {!! Form::close() !!}
     </div>
 @endsection
